8bb177c1f0fc90847ab86e38ecf545d556eb6f37
ArditMucolli <mucolliardit@gmail.com>
2025-01-28 22:10:11 +0100

    fix: fxing the design of the gallery photos

--- app/components/gallery/Gallery.js
+++ app/components/gallery/Gallery.js
@@ -8,51 +8,63 @@ export default function Gallery() {
       </div>
 
       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6 mt-10">
-        <Image
-          src="/assets/gallery/Frame1.png"
-          alt="Image 1"
-          width={350}
-          height={250}
-          className="object-cover rounded-lg"
-        />
-        <Image
-          src="/assets/gallery/Frame2.png"
-          alt="Image 2"
-          width={350}
-          height={250}
-          className="object-cover rounded-lg"
-        />
-        <Image
-          src="/assets/gallery/Frame3.png"
-          alt="Image 3"
-          width={350}
-          height={250}
-          className="object-cover rounded-lg"
-        />
-        <Image
-          src="/assets/gallery/Frame4.png"
-          alt="Image 4"
-          width={350}
-          height={250}
-          className="object-cover rounded-lg"
-        />
-        <Image
-          src="/assets/gallery/Frame5.png"
-          alt="Image 5"
-          width={350}
-          height={250}
-          className="object-cover rounded-lg"
-        />
-        <Image
-          src="/assets/gallery/Frame6.png"
-          alt="Image 6"
-          width={350}
-          height={250}
-          className="object-cover rounded-lg"
-        />
-      </div>
+        <div className="w-full h-[250px]">
+          <Image
+            src="/assets/gallery/Frame1.png"
+            alt="Image 1"
+            width={350}
+            height={250}
+            className="object-cover rounded-lg"
+          />
+        </div>
+        <div className="w-full h-[250px]">
+          <Image
+            src="/assets/gallery/Frame2.png"
+            alt="Image 2"
+            width={350}
+            height={250}
+            className="object-cover rounded-lg"
+          />
+        </div>
+        <div className="w-full h-[250px]">
+          <Image
+            src="/assets/gallery/Frame3.png"
+            alt="Image 3"
+            width={350}
+            height={250}
+            className="object-cover rounded-lg"
+          />
+        </div>
 
-      <div className="flex justify-center mt-10">
+        <div className="w-full h-[250px] mt-16">
+          <Image
+            src="/assets/gallery/Frame4.png"
+            alt="Image 4"
+            width={350}
+            height={250}
+            className="object-cover rounded-lg"
+          />
+        </div>
+        <div className="w-full h-[250px]  mt-56">
+          <Image
+            src="/assets/gallery/Frame5.png"
+            alt="Image 5"
+            width={350}
+            height={250}
+            className="object-cover rounded-lg"
+          />
+        </div>
+        <div className="w-full h-[250px]  mt-16 ">
+          <Image
+            src="/assets/gallery/Frame6.png"
+            alt="Image 6"
+            width={350}
+            height={250}
+            className="object-cover rounded-lg"
+          />
+        </div>
+      </div>
+      <div className="flex justify-center mt-24">
         <button className="w-[158px] h-[50px] text-[rgba(29, 29, 31, 1)] bg-transparent border-2 border-black rounded-full">
           Show More...
         </button>
