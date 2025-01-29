b56991fcaca4a359d4e1f3a4a30f233559131888
ArditMucolli <mucolliardit@gmail.com>
2025-01-29 16:35:28 +0100

    feat: responsive for gallery

--- app/components/gallery/Gallery.js
+++ app/components/gallery/Gallery.js
@@ -3,13 +3,13 @@ import Image from "next/image";
 export default function Gallery() {
   return (
     <div className="p-10 max-w-[1066px] mx-auto">
-      <div className="flex justify-between items-center mt-10">
-        <h1 className="text-[28px] text-[#1D1D1F] dark:text-[#FFFFFF]">
+      <div className="flex justify-between items-center mt-10 ">
+        <h1 className="md:text-[28px] text-[20px] font-semibold text-[#1D1D1F] dark:text-[#FFFFFF]">
           Galerie
         </h1>
       </div>
 
-      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6 mt-10">
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-44 md:gap-6 mt-10">
         <div className="w-full h-[250px]">
           <Image
             src="/assets/gallery/Frame1.svg"
@@ -28,7 +28,7 @@ export default function Gallery() {
             className="object-cover rounded-lg"
           />
         </div>
-        <div className="w-full h-[250px]">
+        <div className="w-full h-[250px] md:mt-0 mt-40">
           <Image
             src="/assets/gallery/Frame3.svg"
             alt="Image 3"
@@ -38,7 +38,7 @@ export default function Gallery() {
           />
         </div>
 
-        <div className="w-full h-[250px] mt-16">
+        <div className="w-full h-[250px] mt-16 hidden md:block">
           <Image
             src="/assets/gallery/Frame4.svg"
             alt="Image 4"
@@ -47,7 +47,7 @@ export default function Gallery() {
             className="object-cover rounded-lg"
           />
         </div>
-        <div className="w-full h-[250px]  mt-56">
+        <div className="w-full h-[250px]  mt-56 hidden md:block">
           <Image
             src="/assets/gallery/Frame5.svg"
             alt="Image 5"
@@ -56,17 +56,17 @@ export default function Gallery() {
             className="object-cover rounded-lg"
           />
         </div>
-        <div className="w-full h-[250px]  mt-16 ">
+        <div className="w-full h-[250px]  mt-16 hidden md:block">
           <Image
             src="/assets/gallery/Frame6.svg"
             alt="Image 6"
             width={350}
             height={250}
-            className="object-cover rounded-lg"
+            className="object-cover rounded-lg "
           />
         </div>
       </div>
-      <div className="flex justify-center mt-24">
+      <div className="flex justify-center md:mt-24 mt-36">
         <button className="w-[158px] h-[50px] text-[rgba(29, 29, 31, 1)] bg-transparent border-2 border-black rounded-full dark:border-[#FFFFFF]">
           Show More...
         </button>
