d632bb0f70e61984596011fd59bbc4c308d6ffb1
ArditMucolli <mucolliardit@gmail.com>
2025-01-29 16:04:52 +0100

    feat: responsive for about page

--- app/components/about/About.js
+++ app/components/about/About.js
@@ -2,18 +2,18 @@ import Module from "./Module";
 
 export default function About() {
   return (
-    <div className="w-[1066px]  mx-auto mt-[500px] mb-36">
-      <h1 className="text-[28px] text-[#1D1D1F] dark:text-[#FFFFFF] text-left">
+    <div className="w-full mx-auto mt-[150px] mb-36 px-6 md:w-[1066px] md:mt-[500px]">
+      <h1 className="text-[20px] sm:text-[24px] md:text-[28px] font-semibold text-[#1D1D1F] dark:text-[#FFFFFF] text-left sm:mt-4 md:mt-8">
         Die ONERI-Softwarelösung
       </h1>
-      <p className="text-[24px] text-[#1D1D1F] dark:text-[#FFFFFF] text-left mt-4 ">
+      <p className="font-medium text-[16px] sm:text-[18px] md:text-[24px] text-[#1D1D1F] dark:text-[#FFFFFF] text-left mt-4 sm:mt-2 md:mt-6">
         ONERI ist eine leistungsstarke Plattform, die entwickelt wurde, um Ihre
         Geschäftsabläufe zu vereinfachen und zu verbessern. Mit einem Fokus auf
         Automatisierung, Organisation und Zusammenarbeit bietet ONERI alles, was
         Sie benötigen, um Teams, Vertrieb und Verträge effizient zu verwalten.
       </p>
       <Module />
-      <p className="text-[24px] text-[#1D1D1F] dark:text-[#FFFFFF] text-left mt-4 l">
+      <p className="font-medium text-[16px] sm:text-[18px] md:text-[24px] text-[#1D1D1F] dark:text-[#FFFFFF] text-left mt-4 sm:mt-2 md:mt-6">
         ONERI optimiert Ihre Arbeitsprozesse und ermöglicht höhere Effizienz,
         bessere Teamarbeit und klügere Entscheidungen – alles in einer einzigen
         Suite!
--- app/components/about/Module.js
+++ app/components/about/Module.js
@@ -3,18 +3,18 @@ import Image from "next/image";
 export default function Module() {
   return (
     <div className="mt-10 mb-10">
-      <h1 className="text-[28px] text-[#1D1D1F] dark:text-[#FFFFFF] text-left">
+      <h1 className="text-[20px] font-semibold md:text-[28px] text-[#1D1D1F] dark:text-[#FFFFFF] text-left">
         Hauptmodule:
       </h1>
       <ul className="flex flex-col gap-6 text-gray-700 ml-5 mt-7">
         <li className=" flex items-center gap-6">
           <Image
-            src="/assets/module/users.svg"
+            src="/assets/module/users2.svg"
             alt="icon"
             width={40}
             height={40}
           />
-          <p className="text-[#1D1D1F] dark:text-[#FFFFFF] text-[24px]">
+          <p className="text-[#1D1D1F] font-medium dark:text-[#FFFFFF] text-[16px] md:text-[24px]">
             Vertragsmanagement: Erstellen, bearbeiten und verwalten Sie Verträge
             mit individuell anpassbaren Vorlagen.
           </p>
@@ -26,7 +26,7 @@ export default function Module() {
             width={40}
             height={40}
           />
-          <p className="text-[#1D1D1F] dark:text-[#FFFFFF] text-[24px]">
+          <p className="text-[#1D1D1F] font-medium dark:text-[#FFFFFF] text-[16px] md:text-[24px]">
             Automatisierte Abrechnung: Berechnen und automatisieren Sie
             Auszahlungen für Mitarbeiter basierend auf abgeschlossenen
             Verträgen.
@@ -39,7 +39,7 @@ export default function Module() {
             width={40}
             height={40}
           />
-          <p className="text-[#1D1D1F] dark:text-[#FFFFFF] text-[24px]">
+          <p className="text-[#1D1D1F] font-medium dark:text-[#FFFFFF] text-[16px] md:text-[24px]">
             Verkaufstracker: Organisieren und verfolgen Sie Leads, prüfen Sie
             die Nähe von Mitarbeitern zu Leads, visualisieren Sie Standorte auf
             einer Karte und importieren Sie Leads.
@@ -52,7 +52,7 @@ export default function Module() {
             width={40}
             height={40}
           />
-          <p className="text-[#1D1D1F] dark:text-[#FFFFFF] text-[24px]">
+          <p className="text-[#1D1D1F] font-medium dark:text-[#FFFFFF] text-[16px] md:text-[24px]">
             Akademie: Entwickeln und teilen Sie Schulungsmaterialien wie Videos,
             Dokumente und Präsentationen mit Teams oder einzelnen Mitarbeitern.
           </p>
@@ -64,7 +64,7 @@ export default function Module() {
             width={40}
             height={40}
           />
-          <p className="text-[#1D1D1F] dark:text-[#FFFFFF] text-[24px]">
+          <p className="text-[#1D1D1F] font-medium dark:text-[#FFFFFF] text-[16px] md:text-[24px]">
             Kommunikationstool: Echtzeit-Chat, Gruppenkanäle, Dateifreigabe und
             Unternehmensnews halten Ihr Team stets verbunden und informiert.
           </p>
@@ -76,7 +76,7 @@ export default function Module() {
             width={40}
             height={40}
           />
-          <p className="text-[#1D1D1F] dark:text-[#FFFFFF] text-[24px]">
+          <p className="text-[#1D1D1F] font-medium dark:text-[#FFFFFF] text-[16px] md:text-[24px]">
             Berichtswesen: Erstellen Sie detaillierte Berichte, um Leistung zu
             analysieren und datenbasierte Entscheidungen zu treffen.
           </p>
