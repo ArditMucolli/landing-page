ec0fd86c3f437cd36529ddaf587a39d288831d67
ArditMucolli <mucolliardit@gmail.com>
2025-01-29 13:50:20 +0100

    feat: implementing dark mode feature

--- app/components/footer/FooterSocials.js
+++ app/components/footer/FooterSocials.js
@@ -1,7 +1,12 @@
+"use client";
+
 import Image from "next/image";
 import Link from "next/link";
+import { useTheme } from "next-themes";
 
 export default function FooterSocials() {
+  const { theme } = useTheme();
+
   return (
     <div className="mb-7 flex flex-col lg:flex-row items-start justify-between gap-4 mt-10 text-left w-full">
       <div className="flex-1">
@@ -12,7 +17,7 @@ export default function FooterSocials() {
           height={35}
         />
       </div>
-      <div className="flex-1 text-[14px] text-[#6E6E73]">
+      <div className="flex-1 text-[14px] text-[#6E6E73] dark:text-[#6E6E73]">
         <p>© 2024 TECH FRAME Deutschland GmbH | www.tframe.de</p>
       </div>
       <div className="flex-1 flex justify-end ">
@@ -20,40 +25,60 @@ export default function FooterSocials() {
           <li>
             <Link href={"https://www.facebook.com/"}>
               <Image
-                src="/assets/footer/facebook.svg"
-                alt="facebook"
+                src={
+                  theme === "light"
+                    ? "/assets/footer/facebook.svg"
+                    : "/assets/icons/facebook.svg"
+                }
+                alt="icon"
                 width={30}
                 height={30}
+                className="ml-3"
               />
             </Link>
           </li>
           <li>
             <Link href={"https://www.instagram.com/"}>
               <Image
-                src="/assets/footer/instagram.svg"
-                alt="instagram"
+                src={
+                  theme === "light"
+                    ? "/assets/footer/instagram.svg"
+                    : "/assets/icons/instagram.svg"
+                }
+                alt="icon"
                 width={30}
                 height={30}
+                className="ml-3"
               />
             </Link>
           </li>
           <li>
             <Link href={"https://www.linkedin.com/"}>
               <Image
-                src="/assets/footer/linkedin.svg"
-                alt="linkedin"
+                src={
+                  theme === "light"
+                    ? "/assets/footer/linkedin.svg"
+                    : "/assets/icons/linkedin.svg"
+                }
+                alt="icon"
                 width={30}
                 height={30}
+                className="ml-3"
               />
             </Link>
           </li>
           <li>
             <Link href={"https://www.youtube.com/"}>
               <Image
-                src="/assets/footer/youtube.svg"
-                alt="youtube"
+                src={
+                  theme === "light"
+                    ? "/assets/footer/youtube.svg"
+                    : "/assets/icons/youtube.svg"
+                }
+                alt="icon"
                 width={30}
                 height={30}
+                className="ml-3"
               />
             </Link>
           </li>
