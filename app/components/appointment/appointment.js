3837b06d67b905117aff83b6cdf43e6503be87be
ArditMucolli <mucolliardit@gmail.com>
2025-01-29 16:20:42 +0100

    feat: responsive for appointment page

--- app/components/appointment/appointment.js
+++ app/components/appointment/appointment.js
@@ -3,8 +3,8 @@ import Image from "next/image";
 export default function Appointment() {
   return (
     <div className="h-[1091px] bg-gradient-to-b from-[#6C1EB0] via-[#ae7bdb] to-[#a866e2] p-10">
-      <div className="w-[1066px] mx-auto flex justify-between items-center mt-10">
-        <h1 className="text-[28px] max-w-[268px] text-left text-[#FFFFFF]">
+      <div className="w-full mx-auto flex flex-row justify-between items-center mt-10 px-4">
+        <h1 className="text-[16px] sm:text-[28px] font-bold max-w-[180px] sm:max-w-[268px] text-left text-[#FFFFFF]">
           Vereinbaren Sie eine 15-minütige Demo
         </h1>
         <Image
@@ -12,8 +12,10 @@ export default function Appointment() {
           alt="icon"
           width={125}
           height={125}
+          className="h-[126px] w-[126px] mt-4 sm:mt-0 sm:ml-4"
         />
       </div>
+
       <div className="flex justify-center mt-10">
         <iframe
           src="https://calendar.google.com/calendar/appointments/schedules/AcZssZ0FsQowoD7jUa5_Hp0yiqmtKXK-HegJhEOnnteGNrR32F8lvVg-vr1Nm91MCILlZyMIj5QfChdR?gv=true"
@@ -21,7 +23,7 @@ export default function Appointment() {
           width="100%"
           height="600"
           frameBorder="0"
-          className="h-[777px] w-[1066px] border rounded-lg shadow-md bg-white"
+          className="md:h-[777px] md:w-[1066px] w-[341px] h-[800px]  border rounded-lg shadow-md bg-white"
           title="Google Calendar Appointment"
           scrolling="no"
         />
