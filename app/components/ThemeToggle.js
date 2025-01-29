6d084b9f7c442a707f15f0062440709d2a334000
ArditMucolli <mucolliardit@gmail.com>
2025-01-29 14:02:04 +0100

    fix: fix dark mode button

--- app/components/ThemeToggle.js
+++ app/components/ThemeToggle.js
@@ -1,8 +1,16 @@
 import { useTheme } from "next-themes";
 import Image from "next/image";
+import { useEffect, useState } from "react";
 
 const ThemeToggle = () => {
   const { theme, setTheme } = useTheme();
+  const [mounted, setMounted] = useState(false);
+
+  useEffect(() => {
+    setMounted(true);
+  }, []);
+
+  if (!mounted) return null;
 
   return (
     <div>
