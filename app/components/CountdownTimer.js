414a71ba1b8b11f7068e1baf8e4758c019788d22
ArditMucolli <mucolliardit@gmail.com>
2025-01-30 09:24:25 +0100

    .

--- app/components/CountdownTimer.js
+++ app/components/CountdownTimer.js
@@ -1,23 +1,15 @@
 "use client";
 
 import Countdown from "react-countdown";
-import { useEffect, useState } from "react";
 
 export default function CountdownTimer({ targetDate }) {
-  const [isClient, setIsClient] = useState(false);
-
-  useEffect(() => {
-    setIsClient(true);
-  }, []);
-
-  if (!isClient) return null;
-
   return (
     <div className="flex gap-4 md:gap-8 items-center justify-center text-xl font-semibold mt-12">
       <Countdown
         date={new Date(targetDate)}
         renderer={({ days, hours, minutes, seconds }) => (
           <>
+            {/* Days */}
             <div className="flex flex-col items-center justify-center">
               <div className="bg-[rgba(255,255,255,0.6)] w-[74px] h-[65px] md:w-[86px] md:h-[75px] flex items-center justify-center rounded-lg">
                 <span className="text-4xl md:text-[40px] text-[#F7A304] bg-gradient-to-r from-[#F7A304] to-[#E5570D] bg-clip-text text-transparent">
@@ -26,6 +18,8 @@ export default function CountdownTimer({ targetDate }) {
               </div>
               <span className="text-[14px] text-[#F2F2F2]">Days</span>
             </div>
+
+            {/* Hours */}
             <div className="flex flex-col items-center justify-center">
               <div className="bg-[rgba(255,255,255,0.6)] w-[74px] h-[65px] md:w-[86px] md:h-[75px] flex items-center justify-center rounded-lg">
                 <span className="text-4xl md:text-[40px] text-[#F7A304] bg-gradient-to-r from-[#F7A304] to-[#E5570D] bg-clip-text text-transparent">
@@ -34,6 +28,8 @@ export default function CountdownTimer({ targetDate }) {
               </div>
               <span className="text-[14px] text-[#F2F2F2]">Hours</span>
             </div>
+
+            {/* Minutes */}
             <div className="flex flex-col items-center justify-center">
               <div className="bg-[rgba(255,255,255,0.6)] w-[74px] h-[65px] md:w-[86px] md:h-[75px] flex items-center justify-center rounded-lg">
                 <span className="text-4xl md:text-[40px] text-[#F7A304] bg-gradient-to-r from-[#F7A304] to-[#E5570D] bg-clip-text text-transparent">
@@ -42,6 +38,8 @@ export default function CountdownTimer({ targetDate }) {
               </div>
               <span className="text-[14px] text-[#F2F2F2]">Minutes</span>
             </div>
+
+            {/* Seconds */}
             <div className="flex flex-col items-center justify-center">
               <div className="bg-[rgba(255,255,255,0.6)] w-[74px] h-[65px] md:w-[86px] md:h-[75px] flex items-center justify-center rounded-lg">
                 <span className="text-4xl md:text-[40px] text-[#F7A304] bg-gradient-to-r from-[#F7A304] to-[#E5570D] bg-clip-text text-transparent">
